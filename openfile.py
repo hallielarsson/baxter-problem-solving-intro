f57618d5609bb8045562b1b82475dcc9c58dc307
halstedlarsson <halsted.larsson@gmail.com>
2015-09-09 06:42:16 -0400

    added a loop so we can add words more easily

--- openfile.py
+++ openfile.py
@@ -1,4 +1,10 @@
-out = "hello"
-out += " "
-out += "world"
+outList = []
+outList.append("hello")
+outList.append(" ")
+outList.append("world")
+out = ""
+i = 1
+while i < len(outList):
+	out += outList[i]
+	i = i + 1
 print(out)
